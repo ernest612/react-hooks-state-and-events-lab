ad20f70947f04d905cea65c76ea6e91bf2968684
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-08-06 19:39:50 +0300

    PUBLISH

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+
+  const [add, addToCart] = useState(false)
+
+  function addToList(){
+    addToCart(!add)
+  }
   return (
-    <li className="">
+    <li className={ add ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToList}>
+        {add ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
